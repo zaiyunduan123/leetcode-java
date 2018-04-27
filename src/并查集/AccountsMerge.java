d49e7072a09a33ba076a21792cf0dba8a2287278
zaiyunduan123 <1778667492@qq.com>
2018-04-27 19:06:43 +0800

    commit

--- src/并查集/AccountsMerge.java
+++ src/并查集/AccountsMerge.java
@@ -7,27 +7,5 @@ import java.util.List;
  */
 public class AccountsMerge {
 
-    public List<List<String>> accountMerge(List<List<String>> accounts){
-
-        return null;
-    }
-    void init(List<List<String>> lists){
-
-        for(List<String> list : lists){
-            for(String s : list){
-//                s.getBytes();
-            }
-        }
-    }
-
-    List<String> find(List<List<String>> list){
-
-        return null;
-
-    }
-
-
-
-
 
 }
